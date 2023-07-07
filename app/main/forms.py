3a0562fd3a2e9f44594a292fd7f6b74c7ac89c79
VitaliiFedin <vitalii.fedinn@gmail.com>
2023-07-07 20:24:10 +0300

    edit profile

--- app/main/forms.py
+++ app/main/forms.py
@@ -1,8 +1,19 @@
 from flask_wtf import FlaskForm
-from wtforms import SubmitField, StringField
-from wtforms.validators import DataRequired
+from wtforms import SubmitField, StringField, TextAreaField
+from wtforms.validators import DataRequired, Length
 
 
 class NameForm(FlaskForm):
     name = StringField('What\'s your name', validators=[DataRequired()])
     submit = SubmitField('Submit')
+
+
+class EditProfileForm(FlaskForm):
+    name = StringField('Real name', validators=[Length(0, 64)])
+    location = StringField('Location', validators=[Length(0, 64)])
+    about_me = TextAreaField('About me')
+    submit = SubmitField('Submit')
+
+
+class EditProfileAdminForm(FlaskForm):
+    pass
